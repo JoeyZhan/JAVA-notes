21d76b2ed0beaa267752ad5a50a11ce4afb283cd
JoeyZhan <33839715+JoeyZhan@users.noreply.github.com>
2018-10-18 19:14:59 +0800

    删除JAVA-source，重新整理上传

--- JAVA-source/Test_Collection_Set.java
+++
@@ -1,9 +0,0 @@
-/*
-	Collection接口的子接口 —— Set接口
-
-		Set 接口
-		
-			因为 Set 和 List 都是继承自 Collection 接口，所以 Set 和 List 中有很多方法是一样的;
-			但List接口中有add, set, indexof方法，
-			而Set接口中却只有add方法，没有set,indexof方法，因为Set是无序不能重复的，不存在某元素具体位置这个概念;
-*/
